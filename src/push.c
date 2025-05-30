8f9ead6561dd28bf66f8376e06afc2ec212cb024
Benjamin <benjaminyahari25@gmail.com>
2025-05-30 20:35:46 +0200

    complies with norminette

--- src/push.c
+++ src/push.c
@@ -1,25 +1,25 @@
 #include "push_swap.h"
 
-static void push(t_stack **src, t_stack **dest)
+static void	push(t_stack **src, t_stack **dest)
 {
-    t_stack *tmp;
+	t_stack	*tmp;
 
-    if (*src == NULL)
-        return;
-    tmp = (*src)->next;
-    (*src)->next = *dest;
-    *dest = *src;
-    *src = tmp;
+	if (*src == NULL)
+		return ;
+	tmp = (*src)->next;
+	(*src)->next = *dest;
+	*dest = *src;
+	*src = tmp;
 }
 
-void do_pa(t_stack **stack_a, t_stack **stack_b)
+void	do_pa(t_stack **stack_a, t_stack **stack_b)
 {
-    push(stack_b, stack_a);
-    ft_putstr("pa\n");
+	push(stack_b, stack_a);
+	ft_putstr("pa\n");
 }
 
-void do_pb(t_stack **stack_a, t_stack **stack_b)
+void	do_pb(t_stack **stack_a, t_stack **stack_b)
 {
-    push(stack_a, stack_b);
-    ft_putstr("pb\n");
+	push(stack_a, stack_b);
+	ft_putstr("pb\n");
 }
--- src/tinysort.c
+++ src/tinysort.c
@@ -1,30 +1,30 @@
 #include "push_swap.h"
 
-static int find_highest_index(t_stack *stack)
+static int	find_highest_index(t_stack *stack)
 {
-    int index;
+	int		index;
 
-    index = stack->index;
-    while (stack)
-    {
-        if (stack->index > index)
-            index = stack->index;
-        stack = stack->next;
-    }
-    return (index);
+	index = stack->index;
+	while (stack)
+	{
+		if (stack->index > index)
+			index = stack->index;
+		stack = stack->next;
+	}
+	return (index);
 }
 
-void tiny_sort(t_stack **stack)
+void	tiny_sort(t_stack **stack)
 {
-    int highest;
+	int		highest;
 
-    if (is_sorted(*stack))
-        return;
-    highest = find_highest_index(*stack);
-    if ((*stack)->index == highest)
-        do_ra(stack);
-    else if ((*stack)->next->index == highest)
-        do_rra(stack);
-    if ((*stack)->index > (*stack)->next->index)
-        do_sa(stack);
+	if (is_sorted(*stack))
+		return ;
+	highest = find_highest_index(*stack);
+	if ((*stack)->index == highest)
+		do_ra(stack);
+	else if ((*stack)->next->index == highest)
+		do_rra(stack);
+	if ((*stack)->index > (*stack)->next->index)
+		do_sa(stack);
 }
